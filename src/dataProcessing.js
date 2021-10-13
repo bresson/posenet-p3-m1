f86e0a314959e9c50fb5d2270c110877ee813e5b
Jesse Braddock <jesse.braddock@potatolondon.com>
2021-10-12 21:06:47 -0400

    completed p3 m1

---
+++ src/dataProcessing.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState, useRef } from "react";
+import * as tf from '@tensorflow/tfjs';
+
+export function processData(rawData) {
+};
---
+++ src/modelTraining.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState, useRef } from "react";
+import * as tf from '@tensorflow/tfjs';
+
+
+function buildModel(numOfFeatures) {
+};
+
+export function runTraining(convertedDatasetTraining, convertedDatasetValidation, numOfFeatures) {
+  console.log('runTraining');
+}
